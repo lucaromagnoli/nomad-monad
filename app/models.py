0e9d84564e87bb7524e448d1104de67dff21b5bc
lucaromagnoli <romagnoli.luca@gmail.com>
2023-01-29 21:07:38 +0000

    add Experience model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Experience(models.Model):
+    company_name = models.CharField(max_length=30)
+    company_url = models.CharField(max_length=30)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    role = models.CharField(max_length=30)
+    description = models.TextField()
+    tech = models.JSONField()
