292c9057856506b7507d21e2026a8d7127d68ea1
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-02 08:53:26 +0000

    add static_root

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_bootstrap5",
     "django_bootstrap_icons",
+    "rest_framework",
     "app",
 ]
 
@@ -113,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
